b895f098a82bfa2b20b67aab59d6ac09b0e0666a
carnellzachary <113737991+carnellzachary@users.noreply.github.com>
2022-11-19 13:06:52 -0500

    minor edit

--- scripts/first_map.js
+++ scripts/first_map.js
@@ -13,4 +13,4 @@ slurpeeMarker.bindPopup("<b>Race Trac</b><br>This is my favorite place to get sl
 L.tileLayer('https://api.mapbox.com/styles/v1/zcarnell/clan79yjz002i15pfcn7imdqz/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiemNhcm5lbGwiLCJhIjoiY2xhbjZ6czZsMDVraTNvcGQ1b2h6Zjc2bCJ9.v-shyASMRnY1z_N6vrlt2w', {
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
-}).addTo(map);
+}).addTo(map1);
